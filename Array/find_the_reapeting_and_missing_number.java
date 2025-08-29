51f39602c8301610f1e9928a1caa74342561ba0e
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-08-30 01:59:52 +0530

    Update find_the_reapeting_and_missing_number.java

--- Array/find_the_reapeting_and_missing_number.java
+++ Array/find_the_reapeting_and_missing_number.java
@@ -35,3 +35,58 @@ class Solution {
         return arr;
     }
 }
+
+
+
+
+***************************************   2nd Approach     *************************************
+
+
+
+
+
+
+import java.util.*;
+class Main{
+    public static void main(String []k){
+        Scanner s1 = new Scanner(System.in);
+        
+        int n = s1.nextInt();
+        
+        int arr[] = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = s1.nextInt();
+        }
+        
+        
+        int freq[] = new int[n+1];
+        for(int i = 0; i < n; i++){
+            freq[arr[i]]++;
+        }
+        
+        int duplicate = -1;
+        int missing = -1;
+        
+        for(int i = 1; i <= n; i++){
+            if(freq[i] == 2){
+                duplicate = i;
+            }
+            
+            if(freq[i] == 0){
+                missing = i;
+            }
+        }
+        
+        int nums[] = new int[2];
+        
+        nums[0] = duplicate;
+        nums[1] = missing;
+        
+        for(int i = 0; i < 2; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
+
+
