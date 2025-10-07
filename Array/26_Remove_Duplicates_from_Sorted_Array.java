e3e0aafc46e5a3f7dcb01c225fba69282bcbf7aa
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-10-08 01:02:48 +0530

    Easy Question

---
+++ Array/26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,24 @@
+class Solutions {
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+
+        int i = 0;
+        int j = 1;
+
+        while(j < n){
+
+            if(nums[j] != nums[i]){
+
+                i++;
+
+                nums[i] = nums[j];
+
+            }
+
+            j++;
+        }
+
+        return i + 1;
+    }
+}
